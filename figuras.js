373e9729fca26ab1ec6dea0d719246edabfa9dde
Katerin713 <kccc0713@gmail.com>
2021-07-28 23:36:58 -0500

    se crean las variables y las formulas para calcular el area y el perimetro del cuadrado

--- figuras.js
+++ figuras.js
@@ -1,1 +1,23 @@
-console.log("Hello world");
+//square 
+
+const squareSide = 5;
+console.log("The square sides measure: " + squareSide + "cm");
+
+const squarePerimeter = squareSide * 4;
+console.log("The perimeter of the square measures: " + squarePerimeter + "cm");
+
+const squareArea = squareSide * squareSide;
+console.log("The area of the square measures: " + squareArea + "cm^2");
+
+//triangle
+
+const triangleSide = 5;
+console.log("The square sides measure: " + squareSide + "cm");
+
+const trianglePerimeter = squareSide * 4;
+console.log("The perimeter of the square measures: " + squarePerimeter + "cm");
+
+const triangleArea = squareSide * squareSide;
+console.log("The area of the square measures: " + squareArea + "cm^2");
+
+//circle
