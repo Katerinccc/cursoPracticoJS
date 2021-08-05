488bb4f6332c113cd250c07ee12eceeb85e759c9
Katerin713 <kccc0713@gmail.com>
2021-08-04 22:11:08 -0500

    Se maqueta en HTML el formulario para el segundo taller, se crea un JS la función para usar los datos del usuario para calcular el precio con descuento y se imprime desde JS a HTML el resultado

--- discount.js
+++ discount.js
@@ -1,6 +1,17 @@
 
-function priceWithDiscountCalculate(price, discount){
+function priceWithDiscount(price, discount){
     discountRate = discount / 100;
     const priceWithDiscount = price * (1- discountRate);
     return priceWithDiscount;
 }
+
+function priceWithDiscountCalculate(){
+    const price = document.getElementById("inputPrice");
+    const discount = document.getElementById("inputDiscount");
+    const priceValue = Number(price.value);
+    const discountValue = Number(discount.value);
+    const priceWithDiscountFinal = priceWithDiscount(priceValue , discountValue);
+    const resultP = document.getElementById("resultPrice");
+    resultP.innerText = "El  precio con un descuento de " + discountValue + "% es de: $" + priceWithDiscountFinal;
+}
+
