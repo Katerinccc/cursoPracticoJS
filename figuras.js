3acd198c9f7ac37bbfc94f3c8a0ad512831233a2
Katerin713 <kccc0713@gmail.com>
2021-08-04 20:28:25 -0500

    Se terminan los estilos para cada figura, se crea una clase adicional para el triangulo de forma que se pueda pedir al usuario todos los valores y se crean las funciones para conectar con JS y que los botones queden funcionales

--- figuras.js
+++ figuras.js
@@ -16,11 +16,11 @@ console.groupEnd();
 console.group("triángulo");
 
 function trianglePerimeter(triangleSide1, triangleSide2, triangleBase){
-    return triangleSide1 + triangleSide2 + triangleBase;
+    return Number(triangleSide1) + Number(triangleSide2) + Number(triangleBase);
 } 
 
 function triangleArea(triangleBase, triangleHeight){
-    return (triangleBase * triangleHeight) / 2
+    return (Number(triangleBase) * Number(triangleHeight)) / 2
 }
 console.groupEnd();
 
@@ -37,20 +37,60 @@ function circlePerimeter(circleRadius){
 }
 
 function circleArea(circleRadius){
-    (Math.pow((circleRadius), 2) * PI).toFixed(2)
+    return (Math.pow(circleRadius, 2) * PI).toFixed(2);
 }
 console.groupEnd();
+
+//square
 
 function squarePerimeterCalculate(){
     const input = document.getElementById("squareInput");
     const value = input.value;
     const perimeter = squarePerimeter(value);
-    alert(perimeter);
+    alert("El perímetro del cuadrado es " + perimeter);
 }
 
 function squareAreaCalculate(){
     const input = document.getElementById("squareInput");
     const value = input.value;
     const area = squareArea(value);
-    alert(area);
+    alert("El área del cuadrado es " +area);
+}
+
+//triangle
+
+function trianglePerimeterCalculate(){
+    const side1 = document.getElementById("triangleInput1");
+    const side2 = document.getElementById("triangleInput2");
+    const base = document.getElementById("triangleInputBase");
+    const value1 = (side1.value);
+    const value2 = (side2.value);
+    const valuebase = (base.value);
+    const perimeter = trianglePerimeter(value1, value2, valuebase);
+    alert("El perímetro del triángulo es " + perimeter);
+}
+
+function triangleAreaCalculate(){
+    const heigth = document.getElementById("triangleInputHeight");
+    const base = document.getElementById("triangleInputBase");
+    const valueHeigth = (heigth.value);
+    const valuebase = (base.value);
+    const area = triangleArea(valuebase, valueHeigth)
+    alert("El perímetro del triángulo es " + area);
+}
+
+//Circle
+
+function circlePerimeterCalculate(){
+    const radius = document.getElementById("circleInput");
+    const valueRadius = Number(radius.value);
+    const perimeter = circlePerimeter(valueRadius);
+    alert("El perímetro del círculo es " + perimeter);
+}
+
+function circleAreaCalculate(){
+    const radius = document.getElementById("circleInput");
+    const valueRadius = Number(radius.value);
+    const area = circleArea(valueRadius);
+    alert("El área del círculo es " + area);
 }
