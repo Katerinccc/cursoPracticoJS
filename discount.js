636a62b5f01d5688d8b7b04f3a28b7c3c0cb5f97
Katerin713 <kccc0713@gmail.com>
2021-08-07 18:15:32 -0500

    Se maqueta HTML para la sección del segundo taller y los estilos respectivos con nuevas clases y rehusando estilos donde es posible, adicional se crea lista desplegable para la sección de cupones y se crean las funciones para calcular el descuento con o sin cupon del precio que el usuario ingrese

--- discount.js
+++ discount.js
@@ -1,9 +1,21 @@
 
-function priceWithDiscount(price, discount){
-    discountRate = discount / 100;
-    const priceWithDiscount = price * (1- discountRate);
-    return priceWithDiscount;
-}
+const coupons = [
+    {
+        discount: 0,
+    },
+    {
+        name: "Platzi",
+        discount: 10,
+    },
+    {
+        name: "Learn",
+        discount: 15,
+    },
+    {
+        name: "Growth",
+        discount: 20,
+    },
+];
 
 function priceWithDiscountCalculate(){
     const price = document.getElementById("inputPrice");
@@ -12,6 +24,15 @@ function priceWithDiscountCalculate(){
     const discountValue = Number(discount.value);
     const priceWithDiscountFinal = priceWithDiscount(priceValue , discountValue);
     const resultP = document.getElementById("resultPrice");
-    resultP.innerText = "El  precio con un descuento de " + discountValue + "% es de: $" + priceWithDiscountFinal;
+    resultP.innerText = "El  precio final con Descuento es de $" + Number(priceWithDiscountFinal).toLocaleString("de-DE", {minimumFractionDigits: 0});;
 }
 
+function priceWithDiscount(price, discount){
+    const user = document.getElementById("inputCoupon");
+    const userCoupon = Number(user.value);
+    const coupon = coupons[userCoupon].discount
+    console.log(coupon);
+    discountRate = (discount + coupon) / 100;
+    const priceWithDiscount = price * (1- discountRate);
+    return priceWithDiscount;
+}    
