5de476ab9b439f4694a412f98631435e988bc20c
Katerin713 <kccc0713@gmail.com>
2021-08-15 13:00:07 -0500

    Se organizan todos los archivos JS del taller 3 en un solo archivo para reutilzar el codigo y se conecta la función para calcular promedio con HTML para usar los datos ingresados por el usuario y crear un array para usar la función de calcular promedio

--- src/calculations.js
+++ src/calculations.js
@@ -1,3 +1,5 @@
+
+//Extraer datos del usuario y convertirlos a un array
 function datalist(datos){
     let inicio=0;
     let userList = [];
@@ -20,11 +22,10 @@ function datalist(datos){
     return userList
 }
 
+
+// Promedio
+
 function averageCalculate(userList){
-    // let sumList = 0;
-    // for (let i = 0; i< list.length; i++ ){
-    //     sumList = sumList + list[i];
-    // }
     const sumList = userList.reduce(
         function(acummValue = 0, newElement){
             return acummValue + newElement;
@@ -34,12 +35,37 @@ function averageCalculate(userList){
     return averageList
 };
 
-document.querySelector(".averageButton").addEventListener("click", ()=>{
-    const datos=document.getElementById("user-inputs").value;
+function average(){
+    const datos = document.getElementById("user-inputs").value;
     let data = datalist(datos);
     const result = averageCalculate(data);
     const resultP = document.getElementById("resultAverage");
-    resultP.innerText = "El  promedio es: " + Number(result).toLocaleString("de-DE", {minimumFractionDigits: 0});;
-})
+    resultP.innerText = "El  promedio es: " + result.toFixed(2);
+}
 
 
+
+//Mediana
+
+function medianCalculate (list) {
+    if (list.length !==0){
+        let median = 0;
+        let index = 0;
+        const sortedList = list.sort((a, b) => a - b);
+        if (sortedList.length % 2 === 0 ){
+            index = sortedList.length / 2;
+            let value1 = list[index - 1];
+            let value2 = list[index];
+            const medianValue1and2 = averageCalculate([
+                value1, value2,])
+            return median = medianValue1and2;
+        } else{
+            index = sortedList.length / 2;
+            index = Math.floor(index);
+            return median = list[index];
+        }
+    }
+    else{
+        console.log("Debes ingresar una lista con valores")
+    }
+}
